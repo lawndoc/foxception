31baae81d57aafec8408ca4268b5534e23dd6359
C.J. May <doctormay6@gmail.com>
2021-02-01 20:56:17 -0600

    removed old scripts

--- startapp.sh
+++ startapp.sh
@@ -2,6 +2,8 @@
 
 # remove X1-lock if it exists
 [[ -f /tmp/.X1-lock ]] && rm /tmp/.X1-lock && echo "X1-lock found, deleting"
+# update to latest firefox version
+apt-get install --only-upgrade -y firefox
 
 # start firefox and restart if closed
 while true
